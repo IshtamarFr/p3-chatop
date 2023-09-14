480c86570a186cd7b440bdf13e7044f833b57d82
IshtamarFr <ishta@ishtamar.fr>
2023-09-14 21:00:05 +0200

    Security installed but disabled - Test OK on getTest()

--- src/main/java/fr/chatop/api/controller/ApiController.java
+++ src/main/java/fr/chatop/api/controller/ApiController.java
@@ -1,9 +1,11 @@
 package fr.chatop.api.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ApiController {
 	@GetMapping("/test")
 	public String getTest() {
