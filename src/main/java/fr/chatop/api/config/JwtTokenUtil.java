acf4781b2c7e29deae1c3b52e0b93daf36aa8062
IshtamarFr <ishta@ishtamar.fr>
2023-09-20 09:53:02 +0200

    Auth/me working as expected

--- src/main/java/fr/chatop/api/config/JwtTokenUtil.java
+++ src/main/java/fr/chatop/api/config/JwtTokenUtil.java
@@ -22,7 +22,7 @@ public class JwtTokenUtil {
 	private String SECRET_KEY = "B27F48AD27BDAAA197327AA39F2718CABAD2987AB3B3C7";
 
 	public String generateAccessToken(User user) {
-		return Jwts.builder().setSubject(String.format("%s,%s", user.getId(), user.getEmail())).setIssuer("CodeJava")
+		return Jwts.builder().setSubject(String.format("%s,%s", user.getId(), user.getEmail())).setIssuer("Ishta")
 				.setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
 				.signWith(SignatureAlgorithm.HS512, SECRET_KEY).compact();
 
--- src/main/java/fr/chatop/api/controller/UserController.java
+++ src/main/java/fr/chatop/api/controller/UserController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.chatop.api.config.JwtTokenFilter;
 import fr.chatop.api.config.JwtTokenUtil;
 import fr.chatop.api.model.AuthResponse;
 import fr.chatop.api.model.User;
@@ -23,6 +25,8 @@ public class UserController {
 	private UserService userService;
 	@Autowired
 	private JwtTokenUtil jwtUtil;
+	@Autowired
+	private JwtTokenFilter jwtTokenFilter;
 
 	@GetMapping("/user/{id}")
 	public ResponseEntity<?> getUser(@PathVariable("id") final long id) {
@@ -41,9 +45,27 @@ public class UserController {
 			String accessToken = jwtUtil.generateAccessToken(candidate);
 			AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
 			return ResponseEntity.ok().body(response);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			return ResponseEntity.status(409).body(e);
 		}
 	}
 
+	@GetMapping("/auth/me")
+	// Endpoint is secured so no need to check if jwt exists and is valid
+	public ResponseEntity<?> getMe(@RequestHeader("Authorization") String jwt) {
+		try {
+			String token = jwt.split(" ")[1].trim();
+			String[] jwtSubject = jwtUtil.getSubject(token).split(",");
+			Long id = Long.parseLong(jwtSubject[0]);
+
+			Optional<User> candidate = userService.getUser(id);
+			if (candidate.isPresent()) {
+				return ResponseEntity.ok().body(candidate);
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(409).body(e);
+		}
+	}
 }
