021d0f66a8a4fa7b76b789a690b4460dbac86129
IshtamarFr <ishta@ishtamar.fr>
2023-09-21 14:41:36 +0200

    418 Teapot working for PUT /rentals/id

--- src/main/java/fr/chatop/api/controller/RentalController.java
+++ src/main/java/fr/chatop/api/controller/RentalController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import fr.chatop.api.config.JwtTokenUtil;
 import fr.chatop.api.model.Rental;
 import fr.chatop.api.service.RentalService;
 
@@ -21,6 +23,9 @@ import fr.chatop.api.service.RentalService;
 public class RentalController {
 	@Autowired
 	private RentalService rentalService;
+
+	@Autowired
+	private JwtTokenUtil jwtUtil;
 
 	@GetMapping("/rentals")
 	public ResponseEntity<?> getAllRentals() {
@@ -38,10 +43,16 @@ public class RentalController {
 	}
 
 	@PostMapping("/rentals/{owner_id}")
-	public ResponseEntity<?> createRental(@RequestParam("picture") MultipartFile multipartFile,
-			@RequestParam("name") String name, @RequestParam("surface") float surface,
-			@RequestParam("price") float price, @RequestParam("description") String description,
-			@PathVariable("owner_id") long ownerId) {
+	public ResponseEntity<?> createRental(
+	//@formatter:off
+		@RequestParam("picture") MultipartFile multipartFile,
+		@RequestParam("name") String name,
+		@RequestParam("surface") float surface,
+		@RequestParam("price") float price,
+		@RequestParam("description") String description,
+		@PathVariable("owner_id") long ownerId
+	//@formatter:on
+	) {
 		try {
 			Rental candidate = new Rental();
 			candidate.setName(name);
@@ -63,14 +74,35 @@ public class RentalController {
 	 * IMPORTANT :
 	 * 1- this PutMapping gets PathVariable for Rental's id
 	 * 2- Owner's id must be gotten from Jwt and checked against expected owner's id
-	 * 3- This method doesn't allow to change picture
+	 * 3- This method doesn't allow to change picture (not a requested feature)
 	 */
 	//@formatter: on
-	public ResponseEntity<?> modifyRental(@RequestParam("name") String name, @RequestParam("surface") float surface,
-			@RequestParam("price") float price, @RequestParam("description") String description,
-			@PathVariable("id") long id) {
+	public ResponseEntity<?> modifyRental(
+	//@formatter: off
+		@RequestParam("name") String name,
+		@RequestParam("surface") float surface,
+		@RequestParam("price") float price,
+		@RequestParam("description") String description,
+		@PathVariable("id") long id,
+		@RequestHeader("Authorization") String jwt
+	//@formatter: on
+	) {
 		try {
-			return ResponseEntity.status(418).build();
+			//we first try to check get the owner's id from jwt owner (already validated)
+			long ownerId=jwtUtil.getIdFromValidToken(jwt);
+			Optional<Rental> candidate = rentalService.getRental(id);
+			if (candidate.isPresent()) {
+				//we now check if owner's id matches with rental's owner's id
+				Rental realCandidate=candidate.get();
+				if (ownerId==realCandidate.getOwnerId()) {
+					//token's owner matches with rental's owner's id
+					return ResponseEntity.status(418).body(candidate);
+				} else {
+					return ResponseEntity.notFound().build();
+				}
+			} else {
+				return ResponseEntity.notFound().build();
+			}
 		} catch (Exception e) {
 			return ResponseEntity.status(409).body(e);
 		}
