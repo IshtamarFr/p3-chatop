42c79be68e3b14b0594c9ff6f497a1c882931f7f
IshtamarFr <ishta@ishtamar.fr>
2023-09-27 11:16:37 +0200

    Now Springfox is not compatible...

--- src/main/java/fr/chatop/api/config/SecurityConfiguration.java
+++ src/main/java/fr/chatop/api/config/SecurityConfiguration.java
@@ -5,16 +5,23 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +29,8 @@ public class SecurityConfiguration {
 
 	@Autowired private UserRepository userRepo;
 	@Autowired private JwtTokenFilter jwtTokenFilter;
+	@Autowired AppConfig appConfig;
+	private PasswordEncoder passwordEncoder= appConfig.passwordEncoder();
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
@@ -29,16 +38,24 @@ public class SecurityConfiguration {
 				"/configuration/security", "/swagger-ui.html", "/webjars/**"));
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(username -> userRepo.findByEmail(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found.")));
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
 	}
 
-	@Override
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public UserDetailsManager users(DataSource dataSource) {
+		UserDetails user = User.builder()
+				.username("email")
+				.password(passwordEncoder.encode("password"))
+				.roles("USER")
+				.build();
+		JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+		users.createUser(user);
+		return users;
 	}
 
 	@Bean
