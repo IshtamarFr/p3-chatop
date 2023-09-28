bae4fdc8f651bcc840bec369d7d7876a4d0404f3
IshtamarFr <ishta@ishtamar.fr>
2023-09-28 20:59:21 +0200

    Swagger is more swag :)

--- src/main/java/fr/chatop/api/config/SwaggerConfig.java
+++ src/main/java/fr/chatop/api/config/SwaggerConfig.java
@@ -2,9 +2,10 @@ package fr.chatop.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +13,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Chatop API")
+                .description("API to take care of rentals all around the worlds")
+                .version("SNAPSHOT 0.0.1")
+                .build();
+    }
+
    @Bean
    public Docket api() {
        return new Docket(DocumentationType.SWAGGER_2)
@@ -19,6 +28,7 @@ public class SwaggerConfig {
                .select()
                .apis(RequestHandlerSelectors.basePackage("fr.chatop.api.controller"))
                .paths(PathSelectors.any())
-               .build();
+               .build()
+               .apiInfo(apiInfo());
    }
 }
