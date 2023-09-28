32ed6b56ee272650790bb5dfe73d5ebcccdf97c5
IshtamarFr <ishta@ishtamar.fr>
2023-09-28 09:42:04 +0200

    Added error handling

--- src/main/java/fr/chatop/api/controller/UserController.java
+++ src/main/java/fr/chatop/api/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@ControllerAdvice
 @CrossOrigin(origins = "*")
 public class UserController {
 	@Autowired private UserService userService=new UserServiceImpl();
