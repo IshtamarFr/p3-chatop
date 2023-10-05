b5182dc641c358ebdeb04b6499a20d31e3292575
IshtamarFr <ishta@ishtamar.fr>
2023-10-05 13:30:40 +0200

    security setup BUT AuthenticationManager

--- src/main/java/fr/chatop/api/config/SecurityConfiguration.java
+++ src/main/java/fr/chatop/api/config/SecurityConfiguration.java
@@ -1,32 +1,32 @@
 package fr.chatop.api.config;
 
 import fr.chatop.api.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
 	@Autowired private UserRepository userRepo;
 	@Autowired private JwtTokenFilter jwtTokenFilter;
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web)-> web.ignoring().requestMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
 				"/configuration/security", "/swagger-ui.html", "/webjars/**");
 	}
 
@@ -36,22 +36,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found.")));
 	}
 
-	@Override
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http.csrf(AbstractHttpConfigurer::disable);
 		
 		//roads to set free
 		http.authorizeHttpRequests((authz) -> authz
-			.antMatchers("/auth/login").permitAll()
-			.antMatchers("/auth/register").permitAll()
-			.antMatchers("/v3/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html","/swagger-ui/**", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html").permitAll()
+			.requestMatchers("/auth/login").permitAll()
+			.requestMatchers("/auth/register").permitAll()
+			.requestMatchers("/v3/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html","/swagger-ui/**", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html").permitAll()
 			.anyRequest().authenticated());
 		
 		http.sessionManagement((session)->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -66,6 +65,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         ));
  
 		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
 		// @formatter:on
 	}
 }
